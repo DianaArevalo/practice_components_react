5fcf9165cfc29e000b41f71c326314c36961d1b5
DianaArevalo <morec9834@gmail.com>
2024-09-19 19:58:08 -0500

    second component map

--- practice_components_react/src/components/MyFirstComponent.jsx
+++ practice_components_react/src/components/MyFirstComponent.jsx
@@ -1,15 +1,50 @@
 
 export const MyFirstComponent = () => {
+
+  const name = "Diana Arevalo";
+  const github_profile = "https://github.com"
+
+  const student = {
+    name: "Diana",
+    last_name: "Arevalo", 
+    mobile: 65877657, 
+    linkedIn_profile: "https://www.linkedin.com/in/diana-arevalo-168b0925b/"
+  }
+
+
   return (
     <>
     <div>MyFirstComponent</div>
-    <h1>Temas de React</h1>
-    <ul>
+     <div className="container bg-info-subtle">
+      <h1>Temas de React</h1>
+      <ul>
         <li>Componentes</li>
         <li>Eventos</li>
         <li>Estados Hooks</li>
         <li>Props</li>
-    </ul>    
+      </ul> 
+     </div>
+
+     <div className="container bg-warning-subtle py-2 px-4">
+      <h1>Datos del docente</h1>
+      <p>Nombre: <strong>{ name }</strong></p>
+      <p>GitHub: <a href="https://github.com">{ github_profile }</a> </p>
+     </div>
+
+     <div className="container bg-success-subtle py-2 ">
+      <h1>Datos del estudiante</h1>
+      <p>Nombre: <strong>{ student.name }</strong></p>
+      <p>Apellido: {student.last_name} </p>
+      <p>Celular: {student.mobile} </p>
+      <p>LinkedIn: <a href={student.linkedIn_profile}></a> </p>
+      <p>GitHub: <a href="https://github.com">{ github_profile }</a> </p>
+     </div>
+
+     <div className="container mt-2 pt-2">
+      <h1>Objeto Completo</h1>
+      <pre>{JSON.stringify(student)}</pre>
+     </div>
+      
     </>    
   )
 }
---
+++ practice_components_react/src/components/SecondComponent.jsx
@@ -0,0 +1,30 @@
+
+
+export const SecondComponent = () => {
+
+  const books = [
+    "Harry Potter y la piedra filosofal",
+    "Harry Potter y la camara secreta",
+    "Harry Potter y el prisionero de Azkaban",
+    "Harry Potter y el caliz de fuego",
+    "Harry Potter y la orden del fenix",
+    "Harry Potter y el misterio del principe",
+    "Harry Potter y las reliquias de la muerte",
+    "Animales fantasticos y donde encontrarlos",
+    "Habitos"
+  ]
+  return (
+   <>
+   <h1>Second component</h1>
+   <div>
+    <ul>
+      { 
+        books.map((book, index) => {
+          return <li key={index}>{book}</li>
+        })
+      }
+    </ul>
+   </div>
+   </>
+  )
+}
